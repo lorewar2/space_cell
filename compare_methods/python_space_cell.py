78b36ffa878f621bd3d64d7c820cd473e6f9d481
lorewar2 <mw8511@live.com>
2026-04-29 23:47:14 -0500

    chang params, skel for testint

---
+++ compare_methods/python_space_cell.py
@@ -0,0 +1,38 @@
+import scanpy as sc
+import pandas as pd
+
+def main():
+
+    return
+
+def load_data():
+    # load cells
+
+    # load counts
+
+    # load glm pca
+
+    # load ground truth 
+
+    # load spatial locations
+    return
+
+def glm_pca_map():
+    # based on the ground truth, check the glm pca for each cluster
+
+    return
+
+def border_finder():
+    # find the bordering cells for each ground truth
+    return
+
+def good_cells_in_cluster():
+    # find some good cells in the ground truth cluster
+    return
+
+def poisson_loss():
+    # poisson loss for each cluster using the cluster
+    return
+
+if __name__ == "__main__":
+    main()
